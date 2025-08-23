9979f7a6c40881dcc89c56d7534ed50131f3b741
Jean Burellier <jean.burellier@gmail.com>
2025-08-23 22:12:37 +0200

    feat: add eslint rule array-callback-return

---
+++ src/eslint.js
@@ -0,0 +1,13 @@
+export default [
+    {
+        rules: {
+            "array-callback-return": [
+                "error", {
+                    "allowImplicit": false,
+                    "checkForEach": true,
+                    "allowVoid": false,
+                },
+            ],
+        },
+    },
+];
