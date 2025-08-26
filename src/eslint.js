bc7cf28ae1855e4542905779ac88d1c0e4d19203
Jean Burellier <jean.burellier@gmail.com>
2025-08-26 08:47:54 +0200

    feat: add eslint rule no-func-assign

--- src/eslint.js
+++ src/eslint.js
@@ -88,6 +88,9 @@ export default [
                     "reportUnusedFallthroughComment": true,
                 },
             ],
+            "no-func-assign": [
+                "error",
+            ],
         },
     },
 ];
