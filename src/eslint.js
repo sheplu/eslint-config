6e62d0f38ae506439a5ab059185cb30b2967bc7b
Jean Burellier <jean.burellier@gmail.com>
2025-08-26 07:48:53 +0200

    feat: add eslint rule no-ex-assign

--- src/eslint.js
+++ src/eslint.js
@@ -78,6 +78,9 @@ export default [
                     "allowObjectPatternsAsParameters": false,
                 },
             ],
+            "no-ex-assign": [
+                "error",
+            ],
         },
     },
 ];
