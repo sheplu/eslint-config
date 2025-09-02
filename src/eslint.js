8563158b152550c76c46f837eb7ef5d0b46466c9
Jean Burellier <jean.burellier@gmail.com>
2025-09-02 22:44:40 +0200

    feat: add eslint rule eqeqeq

--- src/eslint.js
+++ src/eslint.js
@@ -315,6 +315,9 @@ export default [
                     "allowPattern": "",
                 },
             ],
+            "eqeqeq": [
+                "error", "always",
+            ],
         },
     },
 ];
