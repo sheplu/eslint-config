b78b8ba33c55816d3a891b3a55cd63ca70a1bc83
Jean Burellier <jean.burellier@gmail.com>
2025-09-21 21:27:03 +0200

    feat: add jsx-a11y rule anchor-ambiguous-text

--- src/jsx-a11y.js
+++ src/jsx-a11y.js
@@ -4,6 +4,11 @@ export default [
             "jsx-a11y/alt-text": [
                 "error",
             ],
-        }
-    }
-]
+            "jsx-a11y/anchor-ambiguous-text": [
+                "error", {
+                    "words": ['click here', 'here', 'link', 'a link', 'learn more'],
+                },
+            ],
+        },
+    },
+];
