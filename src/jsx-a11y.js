cafdb067147ea13feb710b4454ad5f4141139aac
Jean Burellier <jean.burellier@gmail.com>
2025-09-22 07:49:02 +0200

    feat: add jsx-a11y rule no-access-key

--- src/jsx-a11y.js
+++ src/jsx-a11y.js
@@ -77,6 +77,9 @@ export default [
             "jsx-a11y/mouse-events-have-key-events": [
                 "error",
             ],
+            "jsx-a11y/no-access-key": [
+                "error",
+            ],
         },
     },
 ];
