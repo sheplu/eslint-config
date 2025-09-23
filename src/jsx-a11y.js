cf59fa4f0d7774abfc4bae87895c1b12f6c0b35e
Jean Burellier <jean.burellier@gmail.com>
2025-09-23 17:31:25 +0100

    feat: add jsx-a11y rule prefer-tag-over-role

--- src/jsx-a11y.js
+++ src/jsx-a11y.js
@@ -140,6 +140,9 @@ export default [
                     "allowExpressionValues": true,
                 },
             ],
+            "jsx-a11y/prefer-tag-over-role": [
+                "error",
+            ],
         },
     },
 ];
