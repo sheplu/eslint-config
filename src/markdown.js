9131413de87150a76167ea38b6b36f2212fdde17
Jean Burellier <jean.burellier@gmail.com>
2025-09-24 11:54:27 +0100

    feat: add markdown rule fenced-code-language

---
+++ src/markdown.js
@@ -0,0 +1,11 @@
+export default [
+    {
+        rules: {
+            "markdown/fenced-code-language": [
+                "error", {
+                    "required": [],
+                },
+            ],
+        },
+    },
+];
