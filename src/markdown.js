b8194421177e599dbc18a5fc08799ca0fdcb1585
Jean Burellier <jean.burellier@gmail.com>
2025-09-24 12:01:57 +0100

    feat: add markdown rule heading-increment

--- src/markdown.js
+++ src/markdown.js
@@ -6,6 +6,11 @@ export default [
                     "required": [],
                 },
             ],
+            "markdown/heading-increment": [
+                "error", {
+                    "frontmatterTitle": "",
+                },
+            ],
         },
     },
 ];
