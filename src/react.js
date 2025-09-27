d7c4f96787c344a4769167db4da2f7f530c34de4
Jean Burellier <jean.burellier@gmail.com>
2025-09-27 16:03:58 +0200

    feat: add react rule forward-ref-uses-ref

--- src/react.js
+++ src/react.js
@@ -66,6 +66,9 @@ export default [
                     "checkChildContextTypes": true,
                 },
             ],
+            "react/forward-ref-uses-ref": [
+                "error",
+            ],
         },
     },
 ];
