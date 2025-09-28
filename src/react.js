941a9654ad924921fe706b265ca7673d5e3e3eb2
Jean Burellier <jean.burellier@gmail.com>
2025-09-28 08:26:19 +0200

    feat: add react rule jsx-child-element-spacing

--- src/react.js
+++ src/react.js
@@ -88,6 +88,9 @@ export default [
                     "never": [],
                 },
             ],
+            "react/jsx-child-element-spacing": [
+                "error",
+            ],
         },
     },
 ];
