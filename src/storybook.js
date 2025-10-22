80110eb4cf47497f898a7ced93fe13e6b973970f
Jean Burellier <jean.burellier@gmail.com>
2025-10-22 23:59:27 +0200

    feat: add eslint-storybook rule prefer-pascal-case

--- src/storybook.js
+++ src/storybook.js
@@ -37,6 +37,9 @@ export default [
                     "ignore": [],
                 },
             ],
+            "storybook/prefer-pascal-case": [
+                "error",
+            ],
         },
     },
 ];
