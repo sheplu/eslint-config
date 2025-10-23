9bdcc3fe82916fa5ba629cab5c6b44d0ae8bdb6d
Jean Burellier <jean.burellier@gmail.com>
2025-10-23 08:35:25 +0200

    feat: add eslint-playwright rule expect-expect

---
+++ playwright.js
@@ -0,0 +1,11 @@
+export default [
+    {
+        rules: {
+            "playwright/expect-expect": [
+                "error", {
+                    "assertFunctionNames": [],
+                },
+            ],
+        },
+    },
+];
