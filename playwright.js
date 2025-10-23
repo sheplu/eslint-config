059e1dcaad6c9d2d7f3dbc37d9ac035260a16f71
Jean Burellier <jean.burellier@gmail.com>
2025-10-23 08:54:28 +0200

    feat: add eslint-playwright rule no-eval

--- playwright.js
+++ playwright.js
@@ -31,6 +31,9 @@ export default [
             "playwright/no-element-handle": [
                 "error",
             ],
+            "platwright/no-eval": [
+                "error",
+            ],
         },
     },
 ];
