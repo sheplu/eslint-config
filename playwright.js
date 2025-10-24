d3ea6b47dec2ddaf2c4bfa4b4820ded7288a984d
Jean Burellier <jean.burellier@gmail.com>
2025-10-24 23:16:55 +0200

    feat: add eslint-playwright rule no-networkidle

--- playwright.js
+++ playwright.js
@@ -51,6 +51,9 @@ export default [
             "playwright/no-nested-step": [
                 "error",
             ],
+            "playwright/no-networkidle": [
+                "error",
+            ],
         },
     },
 ];
