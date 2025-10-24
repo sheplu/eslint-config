db1e1bc53c98a8914c843f390e1aff29a868acb5
Jean Burellier <jean.burellier@gmail.com>
2025-10-24 23:28:58 +0200

    feat: add eslint-playwright rule no-wait-for-navigation

--- playwright.js
+++ playwright.js
@@ -66,6 +66,9 @@ export default [
             "playwright/no-useless-not": [
                 "error",
             ],
+            "playwright/no-wait-for-navigation": [
+                "error",
+            ],
         },
     },
 ];
