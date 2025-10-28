93bae8090debdc4672348b52eefbd8b1af5a50ad
Jean Burellier <jean.burellier@gmail.com>
2025-10-28 21:39:33 +0100

    feat: add eslint-jsdoc rule check-syntax

--- src/jsdoc.js
+++ src/jsdoc.js
@@ -55,6 +55,9 @@ export default [
                     "enableFixer": true,
                 },
             ],
+            "jsdonc/check-syntax": [
+                "error",
+            ],
 		},
 	},
 ];
