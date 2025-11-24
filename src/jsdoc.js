af3c0caef9c2194975ce18678341536156acdab5
Jean Burellier <jean.burellier@gmail.com>
2025-11-24 08:47:41 +0100

    feat: add eslint-jsdoc rule ts-prefer-function-type

--- src/jsdoc.js
+++ src/jsdoc.js
@@ -474,6 +474,11 @@ export default [
                     "enableFixer": true,
                 },
             ],
+            "jsdoc/ts-prefer-function-type": [
+                "error", {
+                    "enableFixer": true,
+                },
+            ],
 		},
 	},
 ];
