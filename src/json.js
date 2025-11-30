252d081d00674ccc807bbf6b19d300f20712854e
Jean Burellier <jean.burellier@gmail.com>
2025-11-30 23:25:38 +0100

    feat: add eslint-json rule top-level-interop

--- src/json.js
+++ src/json.js
@@ -23,6 +23,9 @@ export default [
                     "allowLineSeparatedGroups": false,
                 },
             ],
+            "json/top-level-interop": [
+                "error",
+            ],
         },
     },
 ];
