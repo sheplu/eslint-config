da983787a77137d5ee22c3410194fd124f30a270
Jean Burellier <jean.burellier@gmail.com>
2025-12-07 14:38:56 +0100

    feat: add perfectionist rule sort-intersection-types

--- src/perfectionist.js
+++ src/perfectionist.js
@@ -174,6 +174,25 @@ export default [
 					customGroups: [],
 				},
 			],
+			"perfectionist/sort-intersection-types": [
+				"error", {
+					type: 'natural',
+					order: 'asc',
+					fallbackSort: {
+						type: 'unsorted',
+						order: 'asc',
+					},
+					alphabet: '',
+					ignoreCase: false,
+					specialCharacters: 'keep',
+					locales: 'en-US',
+					partitionByComment: false,
+					partitionByNewLine: false,
+					newlinesBetween: 0,
+					groups: [],
+					customGroups: [],
+				},
+			]
 		},
 	},
 ];
